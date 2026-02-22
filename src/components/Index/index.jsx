cba22ee24b51c38618797cc51535a4433954292f
Gabriel Machado <gabrielpiomachado680@gmail.com>
2026-02-22 10:03:16 -0300

    create form and tasks

--- src/components/Index/index.jsx
+++ src/components/Index/index.jsx
@@ -1,8 +1,15 @@
 import { useState } from 'react';
 import './index.css';
 
+// Form
+import { FaPlus } from 'react-icons/fa';
+
+// Tasks
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
 export default function Index() {
   const [newTask, setTask] = useState('');
+  const [tasks, setTasks] = useState(['Fazer café', 'Beber agua', 'Estudar']);
 
   const handleChange = (e) => {
     setTask(e.target.value);
@@ -11,12 +18,25 @@ export default function Index() {
   return (
     <div className="index">
       <h1>To-do List</h1>
-      <h2>{newTask}</h2>
 
-      <form action="#">
-        <input onChange={handleChange} type="text" />
-        <button type="submit">Send</button>
+      <form action="#" className="form">
+        <input onChange={handleChange} type="text" value={newTask} />
+        <button type="submit">
+          <FaPlus />
+        </button>
       </form>
+
+      <ul className="tasks">
+        {tasks.map((task) => (
+          <li key={task}>
+            {task}
+            <div>
+              <FaEdit className="edit" />
+              <FaWindowClose className="delete" />
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
